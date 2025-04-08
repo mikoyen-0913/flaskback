279b3cd769cf512ac6629e21e38f182a7a03cece
hijirimzsiro <cherryou0806@gmail.com>
2025-04-08 21:53:48 +0800

    0408

--- firebase_config.py
+++ firebase_config.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("yaoyaoproject-88907-firebase-adminsdk-fbsvc-e65f9829cc.json")
+cred = credentials.Certificate("yaoyaoproject-88907-firebase-adminsdk-fbsvc-b498692948.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
