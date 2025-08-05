f54086a3db32432bc15d6e3ed9fc2d26ea8c4809
mikoyen-0913 <411630667@o365.tku.edu.tw>
2025-08-05 12:09:30 +0800

    w0805

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ if __name__ == '__main__':
         print("✅ Registered Routes:")
         print(app.url_map)
 
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
